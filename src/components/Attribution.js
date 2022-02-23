3c943fad0bf19bd7287b44d35148e0b67a35c176
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-22 20:41:52 -0400

    Add rel="noreferrer" to anchor tag

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -2,7 +2,11 @@ const Attribution = () => {
   return (
     <p className="attribution">
       Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
         Frontend Mentor
       </a>
       . Coded by <a href="https://nakoyawilson.netlify.app/">Nakoya Wilson</a>.
