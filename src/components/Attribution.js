4cfd63a6c55c58b87e5deb87f8a0013ceb16ddcb
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-22 20:39:18 -0400

    Add name and portfolio url

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -5,7 +5,7 @@ const Attribution = () => {
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
       </a>
-      . Coded by <a href="#">Your Name Here</a>.
+      . Coded by <a href="https://nakoyawilson.netlify.app/">Nakoya Wilson</a>.
     </p>
   );
 };
