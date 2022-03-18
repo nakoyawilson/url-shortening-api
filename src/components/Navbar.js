c41f9b99a2fd2441adf9aaf31cb5d5e67a39ab79
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-17 23:11:50 -0400

    Add mobile navigation functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,60 @@
+import { useState } from "react";
 import ButtonLink from "./ButtonLink";
 import logo from "../images/logo.svg";
 import menu from "../images/menu.svg";
+import close from "../images/close.svg";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavOpen((prev) => !prev);
+    document.querySelector("body").classList.toggle("lock-scroll");
+  };
+
+  const closeMenu = (e) => {
+    if (window.innerWidth < 376) {
+      setNavOpen(false);
+      document.querySelector("body").classList.toggle("lock-scroll");
+    }
+    e.preventDefault();
+  };
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Shortly logo" />
-      <button className="navigation-menu">
-        <img src={menu} alt="" />
+      <button className="navigation-menu" onClick={handleToggle}>
+        <img src={navOpen ? close : menu} alt="" />
       </button>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Features</a>
-        </li>
-        <li>
-          <a href="#">Pricing</a>
-        </li>
-        <li>
-          <a href="#">Resources</a>
-        </li>
-      </ul>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Login</a>
-        </li>
-        <li>
-          <ButtonLink content="Sign Up" />
-        </li>
-      </ul>
+      <div className={`nav-links-wrapper ${navOpen && "open"}`}>
+        <ul className="nav-links">
+          <li>
+            <a href="#" className="nav-link" onClick={closeMenu}>
+              Features
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link" onClick={closeMenu}>
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link" onClick={closeMenu}>
+              Resources
+            </a>
+          </li>
+        </ul>
+        <ul className="nav-links login" onClick={closeMenu}>
+          <li>
+            <a href="#" className="nav-link" onClick={closeMenu}>
+              Login
+            </a>
+          </li>
+          <li>
+            <ButtonLink content="Sign Up" onClick={closeMenu} />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
